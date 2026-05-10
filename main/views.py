e9d6c9bb7aad301e186151c6a852c2ea0b866f3f
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-10 18:37:38 +0300

    feat: CataloView

--- main/views.py
+++ main/views.py
@@ -22,4 +22,66 @@ class IndexView(TemplateView):
             return TemplateResponse(request, 'main/home_content.html', context = context)
         return TemplateResponse(request, self.template_name, context=context)
     
+class CatalogView(TemplateView):
+    template = 'main/base.html'
 
+    FILTER_MAPPING = {
+        'color': lambda queryset, value: queryset.filter(color__iexact = value),
+        'min_price': lambda queryset, value: queryset.filter(price__gte = value),
+        'max_price': lambda queryset, value: queryset.filter(price__lte = value),
+        'size': lambda queryset, value: queryset.filter(product_size__size__name = value)
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = kwargs.get('category_clug')
+        categories = Category.objects.all()
+        products = Product.objects.all().order_by('-created_at')
+        current_category = None
+        if category_slug:
+            current_category = get_object_or_404(Category, slug = category_slug)
+            products=products.filter(category = current_category)
+        
+        query = self.request.GET.get('q')
+        if query:
+            products = products.filter(
+                Q(name__icontains=query) | Q(description__icontains=query)
+            )
+        
+        filter_params = {}
+        for param, filter_func in self.FILTER_MAPPING.items():
+            value = self.request.GET.get(param)
+            if value:
+                products = filter_func(products, value)
+                filter_params[param]=value
+            else:
+                filter_params[param] = ''
+        
+        filter_params['q'] = query or ''
+        context.update({
+            'categories':categories,
+            'products': products,
+            'category_slug': category_slug,
+            'filter_params': filter_params,
+            'sizes': Size.objects.all(),
+            'search_query': query or ''
+        })
+
+        if self.request.GET.get('show_search') == 'true':
+            context['show_search'] = True
+        elif self.request.GET.get('reset_search') == 'true':
+            context['reset_search'] = True
+        
+    
+        return context
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            if context.get('show_search'):
+                return TemplateResponse(request, 'main/search_input.html', context)
+            elif context.get('reset_search'):
+                return TemplateResponse(request, 'main/search_button.html', {})
+            template = 'main/filter_modal.html' if request.GET.get('show_filters') =='true' else 'main/catalog.html'
+            return TemplateResponse(request, template, context)
+        return TemplateResponse(request, self.template_name, context)
+            
