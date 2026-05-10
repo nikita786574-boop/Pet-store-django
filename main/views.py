21f95428176cea6b11a686237ffad1992170486c
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-10 16:10:36 +0300

    feat: IndexView

--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, DetailView
 # Create your views here.
+
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from .models import Category, Product, Size
+from django.db.models import Q
+
+class IndexView(TemplateView):
+    template_name = 'main/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['current_category'] = None
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/home_content.html', context = context)
+        return TemplateResponse(request, self.template_name, context=context)
+    
+
