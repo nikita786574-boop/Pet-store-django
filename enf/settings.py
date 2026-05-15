8c126d571c4b864e2fbad56ddfb0bf1ca91cf28a
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-15 09:28:16 +0300

    feat: зарегестрирован Middleware

--- enf/settings.py
+++ enf/settings.py
@@ -54,6 +54,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+
+    'cart.middleware.CartMiddleware',
 ]
 
 ROOT_URLCONF = 'enf.urls'
