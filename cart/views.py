445d6eaeda70191c48b6c0302c71386cd8896956
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-15 10:32:41 +0300

    feat: AddToCartView

--- cart/views.py
+++ cart/views.py
@@ -22,3 +22,65 @@ class CartMixin:
         cart, created = Cart.objects.get_or_create(
             session_key = request.session.session_key,
         )
+        request.session['cart_id'] = cart.id
+        request.session.modified = True
+        return cart
+
+class AddToCartView(CartMixin, View):
+    @transaction.atomic
+    def post(self, request, slug):
+        cart = self.get_cart(request)
+        product=get_object_or_404(Product, slug=slug)
+
+        form = AddToCartForm(request.POST, product=product)
+        if not form.is_valid():
+            return JsonResponse({
+                'error':'Invalid form data',
+                'errors':form.errors,
+            }, status = 400)
+        size_id = form.cleaned_data.get('size_id')
+        if size_id:
+            product_size = get_object_or_404(
+                ProductSize,
+                id = size_id,
+                product = product
+            )
+        else:
+            product_size = product.product_sizes.filter(stock__gt=0).first()
+            if not product_size:
+                return JsonResponse({
+                    'error': 'No sizes avaliable'
+                }, status = 400)
+        quantity = form.cleaned_data['quantity']
+        if product_size.stock < quantity:
+            return JsonResponse({
+                'error': f'Only {product_size.stock} items avaliable'
+            }, status = 400)
+        
+        existing_item = cart.items.filter(
+            product = product,
+            product_size = product_size,
+
+        ).first()
+
+        if existing_item:
+            total_quantity = existing_item.quantity + quantity
+            if total_quantity> product_size.stock:
+                return JsonResponse({
+                    'error':f'Cannot add {quantity} quantity. Only {product_size.stock-existing_item.quantity} more avaliable.'
+
+                }, status = 400)
+        cart_item = cart.add_product(product, product_size, quantity)
+
+        request.session['cart_id'] = cart.id
+        request.session.modified = True
+
+        if request.headers.get['HX-Request']:
+            return redirect('cart:cart_modal')
+        else:
+            return JsonResponse({
+                'success':True,
+                'total_items':cart.total_items,
+                'message':f"{product.name} был добавлен в корзину.",
+                'cart_item_id': cart_item.id
+            })
